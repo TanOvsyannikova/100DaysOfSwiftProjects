01146f8c1765a3e968c36ea47fd2d18ac472725c
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-17 16:23:51 +0300

    add html page viewer, error alerts and another tab to the UITabBarController

--- Parser/Parser/ViewController.swift
+++ Parser/Parser/ViewController.swift
@@ -10,17 +10,30 @@ import UIKit
 class ViewController: UITableViewController {
     var petitions = [Petition]()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "ok", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
@@ -31,8 +44,8 @@ class ViewController: UITableViewController {
         }
         
     }
-
-   
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
@@ -44,6 +57,12 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
