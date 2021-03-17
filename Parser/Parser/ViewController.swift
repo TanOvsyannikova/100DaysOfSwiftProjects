807e8430579687c97af0094a3ce61985b7445264
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-17 16:50:58 +0300

    feat: filter data by key words, add a credits button

--- Parser/Parser/ViewController.swift
+++ Parser/Parser/ViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
-    
+    var filteredPetitions = [Petition]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filter", style: .plain, target: self, action: #selector(filter))
         
         let urlString: String
         if navigationController?.tabBarItem.tag == 0 {
@@ -24,11 +27,43 @@ class ViewController: UITableViewController {
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                filteredPetitions = petitions
                 return
             }
         }
         showError()
     }
+    
+    @objc func filter() {
+        let ac = UIAlertController(title: "Enter:", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let searchedPetiton = ac?.textFields?[0].text else { return }
+            self?.submit(searchedPetiton)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ searchedPetiton: String) {
+        
+        filteredPetitions.removeAll(keepingCapacity: true)
+        
+        for x in petitions {
+            if x.title.contains(searchedPetiton){
+                filteredPetitions.append(x)
+                tableView.reloadData()
+            }
+        }
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Give some credits", message: "All the data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "ok", style: .default))
+        present(ac, animated: true)
+    }
+    
     
     func showError() {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem", preferredStyle: .alert)
@@ -47,12 +82,12 @@ class ViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
@@ -61,7 +96,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
