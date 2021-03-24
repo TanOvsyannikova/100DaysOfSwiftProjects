1f7d85a984b0c6b0c1eaa806185afaaa9f4c3531
TanOvsyannikova <t.ovsyannikova9@gmail.com>
2021-03-24 22:06:15 +0300

    Initial commit

---
+++ NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Татьяна Овсянникова on 24.03.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ NamesToFaces/NamesToFaces/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Татьяна Овсянникова on 24.03.2021.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell")
+        }
+        return cell
+    }
+
+}
+
